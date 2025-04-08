3cd1abeaed4dfdb195dc02983dda800316ee1a68
Kittypoda <odakrolstad@gmail.com>
2025-04-08 13:57:01 +0200

    footer styled

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white text-center p-4">
-        <Link to="/contact" className="hover:underline">Contact us</Link>
-      <p>&copy; 2025 LULU. All rights reserved.</p>
+    <footer className="bg-red-100 text-red-800 text-center p-7">
+        <Link to="/contact" className="font-inria">Contact us</Link>
+      <p className="font-inria font-thin">&copy; 2025 LULU. All rights reserved.</p>
     </footer>
   );
 };
